088d23ed7affa25f5060bf9e5b94722591ca7da1
Xiang Zhang <soloman817@gmail.com>
2016-02-23 16:17:14 +0800

    test project.

---
+++ Test.Fody/ModuleWeaver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace Test.Fody
+{
+    public class ModuleWeaver
+    {
+        public ModuleDefinition ModuleDefinition { get; set; }
+
+        public Action<string> LogInfo { get; set; }
+
+        public Action<string> LogError { get; set; }
+
+        public void Execute()
+        {
+            LogInfo("Weaving...");
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        unsafe private struct IndicesAndValue
+        {
+            public double Value;
+            public fixed int Indices[8];
+        }
+
+        public static unsafe void MakeIndicesAndValue()
+        {
+            var theStruct = new IndicesAndValue();
+            theStruct.Value = 9.9;
+            theStruct.Indices[0] = 8;
+        }
+
+        static void Main(string[] args)
+        {
+            MakeIndicesAndValue();
+        }
+    }
+}
